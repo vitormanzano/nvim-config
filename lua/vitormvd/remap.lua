745add7768663ad20b0576aa604beebb941b42e5
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-03-10 08:08:48 -0300

    feat: Adding themes and remaps

--- lua/vitormvd/remap.lua
+++ lua/vitormvd/remap.lua
@@ -1,3 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>z", "u")
+
+-- Copy
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
