31a078215e8f3230b14fe0655babf9f428cc96c1
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-04-05 09:04:29 -0300

    feat: Adding harpoon

--- lua/vitormvd/lazy/plugins/colors.lua
+++ lua/vitormvd/lazy/plugins/colors.lua
@@ -1,19 +1,14 @@
 return {
-  "Mofiqul/dracula.nvim",
-  -- Set a high priority to ensure the colorscheme loads before other UI plugins
-  priority = 1000,
-  -- Make the transparency plugin a dependency to ensure it's available
-  dependencies = { "xiyaowong/transparent.nvim" },
-  
-  config = function()
-    -- Enable Dracula's own transparency setting (this is good practice)
-    vim.g.dracula_transparent_bg = true
-
-    -- Load the colorscheme
-    vim.cmd.colorscheme("dracula")
-
-    -- Enable transparent.nvim AFTER the colorscheme is set.
-    -- This command ensures transparency is applied correctly over the theme.
-    vim.cmd.TransparentEnable()
-  end,
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                transparent_background = true,
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end
+    }
 }
